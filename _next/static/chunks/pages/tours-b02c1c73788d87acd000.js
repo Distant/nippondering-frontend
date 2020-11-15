75fbdd9ed76f64576cfbe6ccea0a1f88792be9e2
Phil Oats <phillip.oats@gmail.com>
2020-11-15 14:30:04 +0000

    publish: add social links to footer
    generated from commit 8c1c6feec41544b280b87dd029760ac9294c4872

---
+++ _next/static/chunks/pages/tours-b02c1c73788d87acd000.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[28],{mzPd:function(_,n,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/tours",function(){return o("eSoa")}])}},[["mzPd",1,0,2,3,4,5,6,7,9,13,12,14]]]);
