3b24ae12f0aeccd7cf6deec21c07e34ffe45f900
Phil Oats <phillip.oats@gmail.com>
2021-02-26 13:03:02 +0000

    update formatting

--- src/components/commonProps.tsx
+++ src/components/commonProps.tsx
@@ -1,13 +1,13 @@
 import { HeadingProps, ButtonProps, BoxProps, Text } from "@chakra-ui/core"
 import { Price } from "../types/tour"
-import { FaChevronRight } from 'react-icons/fa';
+import { FaChevronRight } from "react-icons/fa"
 
 export const shadows = [
   { boxShadow: "0 1px 3px rgba(77,73,79,0.12), 0 1px 2px rgba(19,9,70,0.24);" },
   { boxShadow: "0 2px 8px rgba(77,73,87,0.16), 0 2px 4px rgba(19,9,70,0.23)" },
   { boxShadow: "0 6px 14px rgba(77,73,87,0.19), 0 4px 8px rgba(34,12,160,0.14)" },
   { boxShadow: "0 8px 20px rgba(77,73,87,0.2), 0 6px 16px rgba(34,12,160,0.12)" },
-  { boxShadow: "0 19px 38px rgba(77,73,87,0.22), 0 15px 18px rgba(34,12,160,0.12)" }
+  { boxShadow: "0 19px 38px rgba(77,73,87,0.22), 0 15px 18px rgba(34,12,160,0.12)" },
 ]
 
 export const highlightShadows = [
@@ -15,14 +15,28 @@ export const highlightShadows = [
   { boxShadow: "0 3px 20px rgba(60,0,80,0.16), 0 3px 8px rgba(60,0,80,0.32)" },
   { boxShadow: "0 10px 20px rgba(0,0,0.1,0.19), 0 6px 6px rgba(0,0,0,0.23)" },
   { boxShadow: "0 14px 28px rgba(0,0,0.1,0.25), 0 10px 10px rgba(0,0,0,0.22)" },
-  { boxShadow: "0 19px 38px rgba(0,0,0.1,0.30), 0 15px 12px rgba(0,0,0,0.22)" }
+  { boxShadow: "0 19px 38px rgba(0,0,0.1,0.30), 0 15px 12px rgba(0,0,0,0.22)" },
 ]
 
-export const PriceDisplay = ({ symbol, code, amount, size = "md" }: { symbol: string, code: string, amount: number, size?: "md" | "lg" }) => {
+export const PriceDisplay = ({
+  symbol,
+  code,
+  amount,
+  size = "md",
+}: {
+  symbol: string
+  code: string
+  amount: number
+  size?: "md" | "lg"
+}) => {
   return (
     <Text as="span" textStyle="cardBody" mx={[0, 0, 0]} p={0}>
-      <Text as="small" color="purple.500" fontSize={14} paddingLeft="0.25em">{symbol == "US$" ? "$" : symbol}</Text>
-      <Text as="b" color="purple.500" fontSize={size == "md" ? "1.3rem" : "1.6rem"} textAlign="center" >{amount.toLocaleString(code, { maximumFractionDigits: 0, minimumFractionDigits: 0 })} </Text>
+      <Text as="small" color="purple.500" fontSize={14} paddingLeft="0.25em">
+        {symbol == "US$" ? "$" : symbol}
+      </Text>
+      <Text as="b" color="purple.500" fontSize={size == "md" ? "1.3rem" : "1.6rem"} textAlign="center">
+        {amount.toLocaleString(code, { maximumFractionDigits: 0, minimumFractionDigits: 0 })}{" "}
+      </Text>
     </Text>
   )
 }
@@ -42,7 +56,7 @@ export const cardBodyProps = {
   mx: { base: 1, md: 2 },
   color: "gray.600",
   p: "2",
-  letterSpacing: "0.01rem"
+  letterSpacing: "0.01rem",
 }
 
 export const errorTextProps = {
@@ -67,7 +81,7 @@ export const ctaButtonProps: ButtonProps = {
   transition: "all 100ms ease-out",
   _hover: { transform: "scale(1.1)", boxShadow: shadows[2].boxShadow, backgroundColor: "purple.500" },
   _focus: { transform: "scale(1.1)", boxShadow: shadows[2].boxShadow },
-  _active: { backgroundColor: "purple.600" }
+  _active: { backgroundColor: "purple.600" },
 }
 
 export const primaryButtonSolid: ButtonProps = {
@@ -135,7 +149,7 @@ export const primaryButtonText: ButtonProps = {
   padding: 2,
   transition: "all 25ms",
   _hover: { textDecor: "underline", _disabled: { textDecor: "none" }, backgroundColor: "transparent" },
-  _focus: { textDecor: "underline", }
+  _focus: { textDecor: "underline" },
 }
 
 export const breadcrumbProps = {
@@ -143,5 +157,5 @@ export const breadcrumbProps = {
   fontSize: "0.8rem",
   p: 2,
   color: "#555",
-  separator: <FaChevronRight color="#555" size="0.6rem" />
+  separator: <FaChevronRight color="#555" size="0.6rem" />,
 }
